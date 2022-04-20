edb5855c6f842c40418ce3cb7f02ab397c9ba4fc
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-04-20 15:39:10 +0800

    database schema, soon will be migrate to sequilize

---
+++ db.sql
@@ -0,0 +1,17 @@
+create database bot_database; 
+
+use bot_database;
+
+create table anggota (
+  id int primary key auto_increment, 
+  nama varchar(30),
+  status int,
+  phone
+);
+
+create table progress (
+  id int primary key auto_increment,
+  id_user int,
+  progress varchar(250),
+  foreign key (id_user) references anggota(id)
+);
