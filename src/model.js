98a0a5ed2e37ff4cf7484403732ff79d743cdb1f
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-05-26 16:21:21 +0800

    delete model.js

--- src/model.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize({
-  // The `host` parameter is required for other databases
-  host: 'localhost',
-  dialect: 'mysql',
-  storage: './database.sqlite'
-});
