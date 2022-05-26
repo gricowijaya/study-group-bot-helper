3f83e6668440413a13736760b269777a294ea313
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-05-26 16:21:16 +0800

    delete app.js

--- src/app.js
+++
@@ -1,7 +0,0 @@
-const Telegraf = require('telegraf');
-
-// insert the token of the bot
-const bot = new Telegraf('5211773992:AAFNRjRUi7wKAHReCoIHhkQ0t2sKvNFeFbE');
-
-// testing method
-bot.command ()
