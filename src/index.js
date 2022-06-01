2765970192e91ec01b3e576e25cf770af135d08b
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-06-01 16:49:57 +0800

    refator database code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
+// to read .env file
+require('dotenv').config();
+
+// import the node modules
+const token = process.env.TELEGRAM_TOKEN;
 const { Telegraf } = require('telegraf');
 const axios = require('axios');
 const express = require("express");
-const token = process.env.TELEGRAM_TOKEN;
 const bot = new Telegraf(token);
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-require('dotenv').config();
 
-var server = { 
-    port : 3000
-}
+// import database query
+const database = require ('./database.js');
+const botcmd = require ('./botcmd.js');
+
+const server = { port : 3000 }
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -35,7 +40,6 @@ let minutes = date_time.getMinutes();
 let seconds = date_time.getSeconds();
 
 // store help information
-
 const helpMessage = `
 Perintah_perintah yang dimiliki oleh bot ini.
 
