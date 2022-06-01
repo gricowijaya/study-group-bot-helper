c8591b27061f399159f647d8c9482ef2bcd5094e
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-06-01 16:49:47 +0800

    use mysql2 instead of mysql

--- src/database.js
+++ src/database.js
@@ -1,28 +1,41 @@
-const mysql = require("mysql");
+var mysql = require('mysql2');
 
-const db = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
+var db = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.DATABASE_NAME,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.log("Error connecting to Db");
-    return;
-  }
-
-  dataStore = [];
-  result.forEach(item => {
-      dataStore.push({
-      id: item.id,
-      name: item.name,
-      price: item.price
-    })
-  });
-
-  console.log("Connection established");
+db.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
 });
 
-module.exports = db
+
+// db.connect((err) => {
+//   if (err) {
+//     console.log("Error connecting to Db");
+//     return;
+//   }
+
+//   dataStore = [];
+//   result.forEach(item => {
+//       dataStore.push({
+//       id: item.id,
+//       name: item.name,
+//       price: item.price
+//     })
+//   });
+
+//   console.log("Connection established");
+// });
+
+
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: ""
+// });
+
+exports = db
